1bbee4e5a07eb8d55764326ed64f960f9ae1dd13
Able <ablex@AbledeMacBook-Pro.local>
2018-01-07 15:03:19 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,81 @@
+// pages/index/index.js
+var app = getApp()  
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log("用户id"+app.globalData.uid);
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    var that = this
+    return {
+      title: "物业租售管家，让买房卖房更放心。",
+      path: '/pages/index/index',
+      success: function (res) {
+        wx.showShareMenu({
+          shareTicket: '物业租售管家，让买房卖房更放心。',
+          withShareTicket: true
+        })
+      },
+      fail: function (res) {
+        console.log("转发失败")
+      }
+    }
+  }
+})
